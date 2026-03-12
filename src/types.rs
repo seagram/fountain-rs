8fa682bb57a7a5e421a098608d8388c12e6a5727
Cal Seagram <143629409+seagram@users.noreply.github.com>
2026-03-11 20:49:05 -0400

    feat: add type modeling documentation

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,35 @@
+/*
+ * The following type modeling differs from the original C++ implemenation.
+ *
+ * The rust-equivalent of the original C++ implementation is as follows:
+ *
+ * enum ElementType {
+ *      SceneHeading,
+ *      Action
+ *      Character,
+ *      (etc.)
+ * }
+ *
+ * struct Element {
+ *      element_type: ElementType
+ *      text: String,
+ *      is_centered: bool,
+ *      scene_number: Optional<String>
+ *      is_dual_dialogue: bool,
+ *      section_depth: u32
+ * }
+ *
+ * The problem with design is that most Element fields do not apply to most ElementTypes.
+ * For example:
+ * - is_centered is only ever true for certain Action elements.
+ * - is_dual_dialogue is only ever true for certain Dialogue elements.
+ * - scene_number only applies to SceneHeading
+ * - section_number only applies to SceneHeading
+ *
+ * In liue of this apporach, we can used enum variants with named fields.
+ * This means each enum variant only contains fields applicable to them.
+ */
+
 enum Element {
     SceneHeading {
         text: String,
